662a9ebc72b438c9fff1e0a874889d0fde394fea
BrianAnco <ancobrian6@gmail.com>
2022-06-29 18:55:26 -0500

    Modificación Insertar

--- insertar.php
+++ insertar.php
@@ -4,15 +4,22 @@ if ($con->connect_errno) {
 echo 'Error al conectar base de datos: ', $con->connect_error;
 exit();
 }
+$aux = htmlspecialchars($_GET['aux']);
 $temperatura = htmlspecialchars($_GET['temperatura']);
 $humedad_aire = htmlspecialchars($_GET['humedad_aire']);
 $humedad_tierra = htmlspecialchars($_GET['humedad_tierra']);
-$fecha_hora = htmlspecialchars($_GET['fecha_hora']);
+date_default_timezone_set('America/Lima');
+$fecha_hora = strval(date('m-d-Y h:i:s a', time()));
+
 $sql = $con->prepare('INSERT INTO invernadero ( temperatura,humedad_aire, humedad_tierra, fecha_hora) VALUES
 (?, ?, ?,?)');
-$sql->bind_param('ssiss', $temperatura, $humedad_aire,$humedad_tierra, $fecha_hora);
+$sql->bind_param('ssss', $temperatura, $humedad_aire,$humedad_tierra, $fecha_hora);
 $sql->execute();
 echo 'OK\n';
+echo $temperatura;
+echo $humedad_aire;
+echo $humedad_tierra;
+echo $fecha_hora;
 ;
 $con->close();
 ?>
--- listar.php
+++ listar.php
@@ -4,13 +4,14 @@ define('USER','id17752076_dbuser');
 define('PASS','cw?B6Iuxw*VV&2<Y');
 define('DB','id17752076_dbname');
 $con = mysqli_connect(HOST,USER,PASS,DB);
-$sql = "SELECT temperatura, humedad_aire, humedad_tierra, fecha_hora FROM `invernadero`;
+$sql = "SELECT temperatura, humedad_aire, humedad_tierra, fecha_hora FROM `invernadero`";
 $res = mysqli_query($con,$sql);
 $jsonData = array();
 while ($array = mysqli_fetch_array($res)) {
 $row_array['temperatura'] = $array['temperatura'];
 $row_array['humedad_aire'] = $array['humedad_aire'];
 $row_array['humedad_tierra'] = $array['humedad_tierra'];
+$row_array['fecha_hora'] = $array['fecha_hora'];
 array_push($jsonData,$row_array);
 }
 echo json_encode($jsonData);
