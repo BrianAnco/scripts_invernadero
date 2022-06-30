0a9df3f4ae7dd34a3cfa074c18150bc8d9535d7a
BrianAnco <ancobrian6@gmail.com>
2022-06-30 09:42:06 -0500

    Configuración DB para Listar

--- listar.php
+++ listar.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST','localhost');
-define('USER','id17752076_dbuser');
-define('PASS','cw?B6Iuxw*VV&2<Y');
-define('DB','id17752076_dbname');
+define('HOST','us-cdbr-east-06.cleardb.net');
+define('USER','bb7b3ac1261bca');
+define('PASS','16234d12');
+define('DB','heroku_e5362bc152d88fb');
 $con = mysqli_connect(HOST,USER,PASS,DB);
 $sql = "SELECT temperatura, humedad_aire, humedad_tierra, fecha_hora FROM `invernadero`";
 $res = mysqli_query($con,$sql);
@@ -15,5 +15,6 @@ $row_array['fecha_hora'] = $array['fecha_hora'];
 array_push($jsonData,$row_array);
 }
 echo json_encode($jsonData);
+echo "Hola Listar";
 mysqli_close($con);
 ?>
