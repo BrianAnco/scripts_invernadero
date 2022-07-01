fb07441d1de663bd1c1b9adb1b72977edff114db
BrianAnco <ancobrian6@gmail.com>
2022-06-30 22:37:40 -0500

    Modificación del Index 6

--- chillaid/invernadero.php
+++ chillaid/invernadero.php
@@ -90,19 +90,19 @@ Licence URI: https://www.os-templates.com/template-terms
     </div>
     <ul class="pr-charts nospace group center">
       <li class="pr-chart-ctrl" data-animate="false">
-        <div class="pr-chart" data-percent="<?php $var1 ?>"><i></i></div>
+        <div class="pr-chart" data-percent="<?php echo $var1 ?>"><i></i></div>
         <p>Porttitor</p>
       </li>
       <li class="pr-chart-ctrl" data-animate="false">
-        <div class="pr-chart" data-percent=<?php $var1 ?>><i></i></div>
+        <div class="pr-chart" data-percent=<?php echo $var1 ?>><i></i></div>
         <p>Condimentum</p>
       </li>
       <li class="pr-chart-ctrl" data-animate="false">
-        <div class="pr-chart" data-percent="<?php $var2 ?>"><i></i></div>
+        <div class="pr-chart" data-percent="<?php echo $var2 ?>"><i></i></div>
         <p>Sollicitudin</p>
       </li>
       <li class="pr-chart-ctrl" data-animate="false">
-        <div class="pr-chart" data-percent=<?php $var2 ?>><i></i></div>
+        <div class="pr-chart" data-percent=<?php echo $var2 ?>><i></i></div>
         <p>Venenatis</p>
       </li>
     </ul>
